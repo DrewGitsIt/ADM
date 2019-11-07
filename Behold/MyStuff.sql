4c28ae663739819a56c1a06635f36e49cbb96baf
DrewGitsIt <drew.kidwell@appian.com>
2019-11-07 11:18:12 -0500

    mystuff

---
+++ Behold/MyStuff.sql
@@ -0,0 +1,19 @@
+/*
+Data Store: testyDS
+Created: 2019-11-07T16:02:45.676Z
+By: drew.kidwell.s
+Appian Version: 20.1.0.50
+Target Database: Microsoft SQL Server 14.00.3223
+Database Driver: Microsoft JDBC Driver 7.0 for SQL Server 7.0.0.0
+*/
+
+/* UPDATE DDL */
+/* DROP AND CREATE DDL */
+/* WARNING: The DDL commented out below will drop and re-create all tables.
+    drop table [testy];
+
+    create table [testy] (
+        [id] int identity not null,
+        primary key ([id])
+    );
+*/
